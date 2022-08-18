0886bb85fd6f2199117da82cf83951b269c102e5
Ashish yadav <ashish.yadav@sunfox.in>
2022-08-18 13:16:48 +0530

    init

---
+++ testingModule/src/main/java/com/example/testingmodule/TestModule.kt
@@ -0,0 +1,12 @@
+package com.example.testingmodule
+
+class TestModule {
+
+    fun printRandomData() = "testing beta 8..."
+
+    private fun getVersionName() : String {
+//        val packInfo = this.packageManager.getPackageInfo(this.packageName, 0)
+//        return packInfo.versionName
+        return ""
+    }
+}
